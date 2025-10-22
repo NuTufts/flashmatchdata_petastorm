8f86ec7ebc62c1b27adf537ea5db101d6f471ebc
twongjirad <taritree.wongjirad@gmail.com>
2025-10-22 15:46:42 -0400

    save updates for scan over w0 params

--- submit_train_siren_hdf5_data_v2.sh
+++ submit_train_siren_hdf5_data_v2.sh
@@ -3,13 +3,13 @@
 # slurm submission script for making larmatch training data
 
 #SBATCH --job-name=sirendata
-#SBATCH --output=siren_data_fixavepos_multigpu.%j.%N.log
+#SBATCH --output=siren_extbnb_unbsinkdiv_w0_0p03.%j.%N.log
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=4000
 #SBATCH --time=6-00:00:00
 #SBATCH --partition=wongjiradlab
 #SBATCH --gres=gpu:p100:4
-#SBATCH --error=gridlog_train_siren_data_no.%j.%N.err
+#SBATCH --error=griderr_siren_extbnb_unbsinkdiv_w0_0p03.%j.%N.err
 
 container=/cluster/tufts/wongjiradlabnu/larbys/larbys-container/u20.04_cu111_cudnn8_torch1.9.0_minkowski_npm.sif
 WORKDIR=/cluster/tufts/wongjiradlabnu/twongj01/gen2/photon_analysis/flashmatchdata_petastorm/
